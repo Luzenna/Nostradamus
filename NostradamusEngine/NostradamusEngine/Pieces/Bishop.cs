732fc229fa776ffef6d8c2bb0a13af63a8db47a2
vegah <post@fantasista.no>
2014-05-13 20:53:47 +0200

    Issue #6: Moves for Bishop
    Added the possibility to move the bishop around the chess board.

--- NostradamusEngine/NostradamusEngine/Pieces/Bishop.cs
+++ NostradamusEngine/NostradamusEngine/Pieces/Bishop.cs
@@ -1,4 +1,5 @@
 ﻿using NostradamusEngine.Board;
+using NostradamusEngine.Rules;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,12 +35,55 @@ namespace NostradamusEngine.Pieces
 
         public override IEnumerable<Rules.Move> CalculateAllMoves()
         {
-            throw new NotImplementedException();
+            List<Rules.Move> allMoves = new List<Move>();
+
+            // Raycast +1 +1
+            allMoves.AddRange(CalculateMoveInDirection(1, 1));
+            // Raycast +1 -1
+            allMoves.AddRange(CalculateMoveInDirection(1, -1));
+            // Raycast -1 -1
+            allMoves.AddRange(CalculateMoveInDirection(-1, -1));
+            // Raycast -1 +1
+            allMoves.AddRange(CalculateMoveInDirection(-1, 1));
+            return allMoves;
+        }
+
+        private IEnumerable<Rules.Move> CalculateMoveInDirection(Int32 fileAddition, Int32 rankAddition)
+        {
+            var stoppedSearching = false;
+            Int32 fileAdder = fileAddition, rankAdder = rankAddition;
+            while (!stoppedSearching)
+            {
+                var squareToCheck = Game.Board[Square.File + fileAdder, Square.Rank + rankAdder];
+                // Blocked
+                if (squareToCheck == null)
+                {
+                    stoppedSearching = true;
+                }
+                else if (squareToCheck.Piece == null)
+                {
+                    yield return new Move(this, Square, squareToCheck, null);
+                }
+                else if (squareToCheck.Piece.IsWhite == IsWhite)
+                    stoppedSearching = true;
+                else if (squareToCheck.Piece.IsWhite != IsWhite)
+                {
+                    stoppedSearching = true;
+                    yield return new Move(this, Square, squareToCheck, squareToCheck.Piece);
+                }
+                fileAdder += fileAddition;
+                rankAdder += rankAddition;
+            }
         }
 
         public override bool IsLegalMove(Rules.Move move)
         {
-            throw new NotImplementedException();
+            foreach (Move m in CalculateAllMoves())
+            {
+                if (move == m)
+                    return true;
+            }
+            return false;
         }
     }
 }
